61a023bd3b29c31a0beaf1feeb04a6f2936c0c3f
DinaDi021 <di.serdyuck0201@gmail.com>
2023-07-05 21:48:04 +0300

    hw_2.1

--- src/components/PostInfo/PostInfo.js
+++ src/components/PostInfo/PostInfo.js
@@ -1,11 +1,11 @@
 import styles from './Posts.module.css';
 const PostInfo = (props) => {
-    const { post } = props;
+    const { post, handleСlick } = props;
     return (
         <div className={styles.wrapper}>
             <h2>id: {post.id}</h2>
             <h2>{post.title}</h2>
-            <button>More info</button>
+            <button onClick={()=>handleСlick(post)}>More info</button>
         </div>
     )
 }
--- src/components/PostsComponent/PostsComponent.js
+++ src/components/PostsComponent/PostsComponent.js
@@ -1,12 +1,24 @@
 import PostInfo from "../PostInfo/PostInfo.js";
 import {posts} from "../../assets/Posts.js";
+import {useState} from "react";
+
 
 const PostsComponent = () => {
     const one = 'one';
+    const [infoText, setInfoText] = useState('');
+    const handleСlick = (post) => {
+        alert(`        userId - ${post.userId}, Id - ${post.id}, 
+        Title:${post.title}, 
+        Body:${post.body}`);
+    }
     return (
         <>
             {posts.map((post) =>
-                <PostInfo key={post.id} post={post}/>
+                <PostInfo key={post.id}
+                          post={post}
+                          handleСlick={handleСlick}
+                          infoText={infoText}
+                />
             )}
         </>
     )
