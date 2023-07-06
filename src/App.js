f5a7130afbb1a8621df37bb414cff016710a128f
DinaDi021 <di.serdyuck0201@gmail.com>
2023-07-06 19:31:41 +0300

    hw_2.2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShipContainer from "./components/ShipComponent/ShipContainer";
 
 const App = () => {
   return (
-      <div>
+      <div className={'app'}>
 <ShipContainer />
       </div>
   );
