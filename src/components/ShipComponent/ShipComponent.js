4fc1d81f05a91ac3a093bc1ac0bccefd927771ad
DinaDi021 <di.serdyuck0201@gmail.com>
2023-07-06 19:14:49 +0300

    hw_2.2

--- src/components/ShipComponent/ShipComponent.js
+++ src/components/ShipComponent/ShipComponent.js
@@ -2,7 +2,7 @@ const shipComponent = (props) => {
     const { ship, handleСlick } = props;
     return (
         <div className={styles.wrapper}>
-            <h2>mission name: {post.id}</h2>
+            <h2>mission name: {ship}</h2>
             <h2>launch_year: {post.title}</h2>
             <h2>mission_patch_small: {post.title}</h2>
             <button onClick={()=>handleСlick(post)}>More info</button>
--- src/components/ShipComponent/ShipContainer.js
+++ src/components/ShipComponent/ShipContainer.js
@@ -1,20 +1,23 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import ShipComponent from "./ShipComponent.js";
 
 const ShipContainer = () => {
-let allShips = [];
 
 const [ships, setShips] = useState([])
 
-    fetch('https://api.spacexdata.com/v3/launches/')
-        .then(response => response.json())
-        .then((ships) => setShips(ships))
-        .catch((errors) => console.log(errors.response))
+    useEffect(()=>{
+        fetch('https://api.spacexdata.com/v3/launches/')
+            .then(response => response.json())
+            .then((ships) => setShips(ships))
+            .catch((errors) => console.log(errors.response))
+        },[])
+
 
     return (
         <>
-            {ships.map((ships, flight_number) => {
+            {/*{ships.map((ship) => {*/}
 
-            })}
+            {/*})}*/}
         </>
     );
 };
