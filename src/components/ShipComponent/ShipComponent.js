fc20ad3a40e0ae8c9df4430c414edee7c7c55a33
DinaDi021 <di.serdyuck0201@gmail.com>
2023-07-09 15:52:17 +0300

    hw 2.2

--- src/components/ShipComponent/ShipComponent.js
+++ src/components/ShipComponent/ShipComponent.js
@@ -1,12 +1,12 @@
 import styles from './Ship.module.css'
 
-const ShipComponent = props => {
-    const { ship } = props;
+const ShipComponent = ({ship}) => {
+    const { mission_name, launch_year} = ship;
 
     return (
         <div className={styles.container}>
-            <h2>mission name: {ship.mission_name}</h2>
-            <h2>launch_year: {ship.launch_year}</h2>
+            <h2>mission name: {mission_name}</h2>
+            <h2>launch_year: {launch_year}</h2>
             <img src={ship.links.mission_patch_small} alt="mission patch" />
         </div>
     );
