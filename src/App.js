c7c4ca070c50ca1aa18204846ee21ed5937b7e9d
DinaDi021 <di.serdyuck0201@gmail.com>
2023-07-13 22:51:31 +0300

    hw 4.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import LoginForm from "./components/LoginForm/LoginForm";
 
 const App = () => {
   return (
       <div>
-        App
+        <LoginForm/>
       </div>
   );
 };
