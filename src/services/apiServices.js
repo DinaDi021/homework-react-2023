471e5961ae8893000d596c02344bfcfa76d618ea
DinaDi021 <di.serdyuck0201@gmail.com>
2023-07-18 19:48:26 +0300

    hw 5.3

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,15 +1,13 @@
-import React from "react";
 import axios from "axios";
 const {REACT_APP_BASEURL} = process.env;
 
 export const CarsServices = {
-    getCars: async () => {
+    getCars: async (setCars) => {
         try {
-            let response = await axios.get();
-            return response.data;
-        } catch (e) {
-            console.error(e);
+            let response = await axios.get(REACT_APP_BASEURL);
+            setCars(response.data)
+        } catch (error) {
+            console.log(error);
         }
-    },
-
-};
+    }
+}
