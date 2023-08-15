92c7af3c6e66588b0af3f07723c059027fdd74d0
DinaDi021 <di.serdyuck0201@gmail.com>
2023-08-15 16:30:04 +0300

    hw_10

--- src/redux/reducers/store.js
+++ src/redux/reducers/store.js
@@ -1,12 +1,19 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
+import thunk from "redux-thunk";
 
 import {carsReducer} from "./carsReducer";
-import thunk from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension";
+
 
 const rootReducer = combineReducers({
 cars: carsReducer
 });
-const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const composeEnhancers = composeWithDevTools({
+    trace: true,
+    traceLimit: 25
+})
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export {
     store
